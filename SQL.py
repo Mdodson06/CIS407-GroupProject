b3438cdd4392d24d22e040ba18b2ed17273c33a7
Mdodson06 <mariadodson8@gmail.com>
2024-11-23 23:34:01 -0600

    Finished/Fixed basic create/insert/select
    Made foreign keys actually connected, made inserts and selects for remaining tables
    DateTime still allows NULL

--- SQL.py
+++ SQL.py
@@ -4,9 +4,9 @@ cursor = ''
 con = ''
 
 def create_db():
-    #TO DO: Finish CREATES, make DateTimes NOT NULL
+    #TO DO: make DateTimes NOT NULL
     #currently not set to NOT NULL because I don't know how to insert a DateTime :>
-    #Not tested past big space in executescript 
+    
     global con
     global cursor
     con = sqlite3.connect("Hotel.db")
@@ -31,8 +31,10 @@ def create_db():
             booking_id INTEGER PRIMARY KEY AUTOINCREMENT,
             guest_id INTEGER NOT NULL,
             room_number INTEGER NOT NULL,
-            check_in_date DATETIME ,
-            check_out_date DATETIME 
+            check_in_date DATETIME,
+            check_out_date DATETIME,
+            FOREIGN KEY(guest_id) REFERENCES Guest(guest_id),
+            FOREIGN KEY(room_number) REFERENCES Room(room_id)
             );
 
 
@@ -51,7 +53,8 @@ def create_db():
             service_staff_id INTEGER,
             request TEXT NOT NULL,
             request_cost DOUBLE(8,2),
-            request_datetime DATETIME
+            request_datetime DATETIME,
+            FOREIGN KEY(booking_id) REFERENCES Booking(booking_id)
             );
     CREATE TABLE IF NOT EXISTS Staff(
             staff_id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -60,10 +63,12 @@ def create_db():
             password TEXT NOT NULL
             );
     CREATE TABLE IF NOT EXISTS ServiceStaff(
-            Servicestaff_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            servicestaff_id INTEGER PRIMARY KEY AUTOINCREMENT,
             staff_id INTEGER NOT NULL,
-            service_id TEXT NOT NULL,
-            filled_datetime DATETIME
+            service_id INTEGER NOT NULL,
+            filled_datetime DATETIME,
+            FOREIGN KEY(staff_id) REFERENCES Staff(staff_id),
+            FOREIGN KEY(service_id) REFERENCES Service(service_id)
             );
     
 
@@ -72,6 +77,8 @@ def create_db():
         '''
         )
     con.commit()
+
+#### Inserts #### 
 def insert_guest(valueList):
     cursor.execute(
         "INSERT INTO Guest(name, phone_number, email, password) VALUES (?, ?, ?,?)",
@@ -82,14 +89,43 @@ def insert_room(valueList):
         "INSERT INTO Room(room_number, type, price) VALUES (?, ?, ?)",
         (valueList[0], valueList[1], valueList[2])
     )
-    con.commit()
-    
+    con.commit()  
 def insert_booking(valueList):
     cursor.execute(
         "INSERT INTO Booking(guest_id, room_number) VALUES (?, ?)",
         (valueList[0], valueList[1])
+    )    
+    con.commit()
+
+
+
+    
+def insert_payment(valueList):
+    cursor.execute(
+        "INSERT INTO Payment(booking_id, payment_method, total_cost) VALUES (?, ?, ?)",
+        (valueList[0], valueList[1], valueList[2])
+    )
+def insert_service(valueList):
+    cursor.execute(
+        "INSERT INTO Service(booking_id, request) VALUES (?, ?)",
+        (valueList[0], valueList[1])
+    )
+    con.commit()  
+def insert_staff(valueList):
+    cursor.execute(
+        "INSERT INTO Staff(name, email, password) VALUES (?, ?, ?)",
+        (valueList[0], valueList[1],valueList[2])
     )
     con.commit()
+def insert_servicestaff(valueList):
+    cursor.execute(
+        "INSERT INTO ServiceStaff(staff_id, service_id) VALUES (?, ?)",
+        (valueList[0], valueList[1])
+    )
+    con.commit()
+
+
+#### Select alls ####
 def select_all_guest():
     cursor.execute("SELECT * FROM Guest")
     print("Guest:", cursor.fetchall())
@@ -99,6 +135,19 @@ def select_all_room():
 def select_all_booking():
     cursor.execute("SELECT * FROM Booking")
     print("Booking:",cursor.fetchall())
+def select_all_payment():
+    cursor.execute("SELECT * FROM Payment")
+    print("Payment:",cursor.fetchall())
+def select_all_service():
+    cursor.execute("SELECT * FROM Service")
+    print("Service:", cursor.fetchall())
+def select_all_staff():
+    cursor.execute("SELECT * FROM Staff")
+    print("Staff:",cursor.fetchall())
+def select_all_servicestaff():
+    cursor.execute("SELECT * FROM ServiceStaff")
+    print("ServiceStaff:",cursor.fetchall())
+
 
 
 def drop_tables():
@@ -114,16 +163,33 @@ def drop_tables():
     con.commit()
  
 if __name__ == "__main__":
-    print("Hello World!")
     create_db()
-    
-    select_all_guest()
-    insert_guest(["GuestName","GuestNumber","GuestEmail","Password!"])
-    insert_room([414,"RoomType",5.12])
-    insert_booking([1,414])
     select_all_guest()
     select_all_room()
     select_all_booking()
+    select_all_payment()
+    select_all_service()
+    select_all_staff()
+    select_all_servicestaff()
+    
+    insert_guest(["GuestName","GuestNumber","GuestEmail","Password!"])
+    insert_room([414,"RoomType",5.12])
+    insert_booking([1,414])
+    insert_payment([1,"Method",5.10])
+    insert_service([1,"Request"])
+    insert_staff(["StaffName", "StaffEmail", "StaffPass!"])
+    insert_servicestaff([1,1])
+
+    print("~~~~~~")
+    
+    select_all_guest()
+    select_all_room()
+    select_all_booking()
+    select_all_payment()
+    select_all_service()
+    select_all_staff()
+    select_all_servicestaff()
+    
     drop_tables()
     
     con.close()
